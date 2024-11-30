52ceddb9cccf948de4fa4aa3278cd84775847afb
waizin28 <waizinlinn.08@gmail.com>
2024-11-29 18:42:54 -0600

    folders setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Result from './pages/Result';
+import BuyCredit from './pages/BuyCredit';
+import Navbar from './components/Navbar';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/result' element={<Result />} />
+        <Route path='/buy' element={<BuyCredit />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/BuyCredit.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BuyCredit = () => {
+  return <div>BuyCredit</div>;
+};
+
+export default BuyCredit;
