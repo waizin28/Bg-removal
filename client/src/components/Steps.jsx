0ccbbcdefcc5b18322fa9988b3dfa5c25e2fac00
waizin28 <waizinlinn.08@gmail.com>
2024-12-01 00:45:52 -0600

    finished steps comp

--- client/src/components/Steps.jsx
+++ client/src/components/Steps.jsx
@@ -7,14 +7,40 @@ const Steps = () => {
       <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent '>
         Steps to remove background <br /> image in seconds
       </h1>
+
       <div className='flex items-start flex-wrap gap-4 mt-16 xl:mt-24 justify-center'>
+        {/* Step 1 */}
         <div className='flex items-start gap-4 bg-white border drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500'>
-          <img src={assets.upload_icon} alt='' />
+          <img className='max-w-9' src={assets.upload_icon} alt='' />
           <div>
-            <p>Upload Image</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
-              harum reiciendis!
+            <p className='text-xl font-medium'>Upload Image</p>
+            <p className='text-sm text-neutral-500 mt-1'>
+              Lorem ipsum dolor sit amet consectetur <br />
+              adipisicing elit. Fuga harum reiciendis!
+            </p>
+          </div>
+        </div>
+
+        {/* Step 2 */}
+        <div className='flex items-start gap-4 bg-white border drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500'>
+          <img className='max-w-9' src={assets.remove_bg_icon} alt='' />
+          <div>
+            <p className='text-xl font-medium'>Remove Background</p>
+            <p className='text-sm text-neutral-500 mt-1'>
+              Lorem ipsum dolor sit amet consectetur <br />
+              adipisicing elit. Fuga harum reiciendis!
+            </p>
+          </div>
+        </div>
+
+        {/* Step 3 */}
+        <div className='flex items-start gap-4 bg-white border drop-shadow-md p-7 pb-10 rounded hover:scale-105 transition-all duration-500'>
+          <img className='max-w-9' src={assets.download_icon} alt='' />
+          <div>
+            <p className='text-xl font-medium'>Download Image</p>
+            <p className='text-sm text-neutral-500 mt-1'>
+              Lorem ipsum dolor sit amet consectetur <br />
+              adipisicing elit. Fuga harum reiciendis!
             </p>
           </div>
         </div>
